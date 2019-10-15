fc6977f735440beb0924363cf9fa036a0abac407
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-10-14 18:38:53 -0700

    tests done

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { render } from '@testing-library/react';
 import * as rtl from '@testing-library/react';
+import Navbar from './components/Navbar'
+import useDarkMode from './hooks/useDarkMode.js'
 
-test('App renders without crashing', () => {
+it('App renders without crashing', () => {
 	render(<App />);
 });
 
+it('Navbar renders without crashing', () => {
+	render(<Navbar />);
+});
+
+it('useDarkMode renders without crashing', () => {
+	render(<useDarkMode />)
+})
+
+it("renders players in app", () => {
+	const app = rtl.render(<App />);
+	app.getByText(/players/i);
+});
